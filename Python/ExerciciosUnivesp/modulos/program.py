bf38a67e4102a562595e8f227e9e239d729527ee
Thiago Saraiva <thesars.info@gmail.com>
2022-01-10 16:10:27 -0300

    Semana 4 de python

--- Python/ExerciciosUnivesp/modulos/program.py
+++ Python/ExerciciosUnivesp/modulos/program.py
@@ -5,9 +5,9 @@ import math as m
 mensagemUm()
 imp2.mensagemTres("Ola um teste")
 m.sqrt(4)
+
 n = 0
-def funcaorecursiva(n):
-    
+def funcaorecursiva(n):    
     if (n < 10):      
         print(n)  
         funcaorecursiva(n + 1)
